4acfa620f49ec75230a486d28cac97fb4e44368f
Juan Pablo <juanpablo9717@gmial.com>
2024-06-16 08:41:11 -0500

    react-native-paper added

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 // import {HelloWorld} from './src/presentation/screens/HelloWorld';
 import {CounterScreen} from './src/presentation/screens/CounterScreen';
+import {PaperProvider} from 'react-native-paper';
 
 export const App = () => {
   return (
-    // eslint-disable-next-line react-native/no-inline-styles
-    <SafeAreaView style={{flex: 1}}>
-      {/* <HelloWorld /> */}
-      <CounterScreen />
-    </SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView style={{flex: 1}}>
+        {/* <HelloWorld /> */}
+        <CounterScreen />
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
